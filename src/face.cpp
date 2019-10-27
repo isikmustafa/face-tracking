e8ae7787103265d68890b98e283d279a06467703
Mustafa Işık <mustafaisikmetu@gmail.com>
2019-10-27 14:15:14 +0100

    dynamic part of vertex buffer is updated via cuda-gl interop now

--- src/face.cpp
+++ src/face.cpp
@@ -1,5 +1,6 @@
 #include "face.h"
 #include "glsl_program.h"
+#include "util.h"
 
 #include <assert.h>
 #include <fstream>
@@ -11,20 +12,19 @@ Face::Face(const std::string& filename)
 	std::string str_dummy;
 	file >> str_dummy;
 
-	int number_of_vertices;
 	int number_of_faces;
 	int int_dummy;
-	file >> number_of_vertices;
+	file >> m_number_of_vertices;
 	file >> number_of_faces;
 	file >> int_dummy;
 
-	std::vector<glm::vec3> positions(number_of_vertices);
-	std::vector<glm::vec3> colors(number_of_vertices);
-	std::vector<glm::vec2> tex_coords(number_of_vertices);
+	std::vector<glm::vec3> positions(m_number_of_vertices);
+	std::vector<glm::vec3> colors(m_number_of_vertices);
+	std::vector<glm::vec2> tex_coords(m_number_of_vertices);
 	m_number_of_indices = 3 * number_of_faces;
 	std::vector<unsigned int> indices(m_number_of_indices);
 	constexpr float mesh_scale = 1 / 1000000.0f;
-	for (int i = 0; i < number_of_vertices; ++i)
+	for (int i = 0; i < m_number_of_vertices; ++i)
 	{
 		file >> positions[i].x >> positions[i].y >> positions[i].z;
 		positions[i] *= mesh_scale;
@@ -42,9 +42,16 @@ Face::Face(const std::string& filename)
 	}
 	file.close();
 
-	int positions_byte_size = number_of_vertices * sizeof(glm::vec3);
-	int colors_byte_size = number_of_vertices * sizeof(glm::vec3);
-	int tex_coords_byte_size = number_of_vertices * sizeof(glm::vec2);
+	int positions_byte_size = m_number_of_vertices * sizeof(glm::vec3);
+	int colors_byte_size = m_number_of_vertices * sizeof(glm::vec3);
+	int tex_coords_byte_size = m_number_of_vertices * sizeof(glm::vec2);
+
+	//We will only update position and color of vertices. In order not to copy the constant texture coordinates,
+	//we dont allocate memory for them.
+	CHECK_CUDA_ERROR(cudaMalloc(&m_face_data_gpu, positions_byte_size + colors_byte_size));
+	CHECK_CUDA_ERROR(cudaMemcpy(m_face_data_gpu                           , positions.data(), positions_byte_size, cudaMemcpyHostToDevice));
+	CHECK_CUDA_ERROR(cudaMemcpy(m_face_data_gpu + m_number_of_vertices * 3, colors.data()   , colors_byte_size   , cudaMemcpyHostToDevice));
+
 	glGenVertexArrays(1, &m_vertex_array);
 	glGenBuffers(1, &m_vertex_buffer);
 	glGenBuffers(1, &m_index_buffer);
@@ -57,10 +64,9 @@ Face::Face(const std::string& filename)
 
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertex_buffer);
 	glBufferData(GL_ARRAY_BUFFER, positions_byte_size + colors_byte_size + tex_coords_byte_size, nullptr, GL_STATIC_DRAW);
-
-	glBufferSubData(GL_ARRAY_BUFFER, 0                                     , positions_byte_size , positions.data());
-	glBufferSubData(GL_ARRAY_BUFFER, positions_byte_size                   , colors_byte_size    , colors.data());
+	//Only copy texture coordinate information via glBufferSubData. Others will be updated via cuda-gl interop.
 	glBufferSubData(GL_ARRAY_BUFFER, positions_byte_size + colors_byte_size, tex_coords_byte_size, tex_coords.data());
+	updateVertexBuffer();
 
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
@@ -120,6 +126,23 @@ Face::~Face()
 		glDeleteVertexArrays(1, &m_vertex_array);
 		m_vertex_array = 0;
 	}
+	if (m_face_data_gpu)
+	{
+		CHECK_CUDA_ERROR(cudaFree(m_face_data_gpu));
+	}
+}
+
+void Face::updateVertexBuffer()
+{
+	CHECK_CUDA_ERROR(cudaGraphicsGLRegisterBuffer(&m_resource, m_vertex_buffer, cudaGraphicsRegisterFlagsWriteDiscard));
+
+	CHECK_CUDA_ERROR(cudaGraphicsMapResources(1, &m_resource, 0));
+	void* vertex_buffer_ptr;
+	size_t size;
+	CHECK_CUDA_ERROR(cudaGraphicsResourceGetMappedPointer(&vertex_buffer_ptr, &size, m_resource));
+	CHECK_CUDA_ERROR(cudaMemcpy(vertex_buffer_ptr, m_face_data_gpu, m_number_of_vertices * sizeof(glm::vec3) * 2, cudaMemcpyDeviceToDevice));
+
+	CHECK_CUDA_ERROR(cudaGraphicsUnmapResources(1, &m_resource, 0));
 }
 
 void Face::draw(const GLSLProgram& program) const
--- src/face.h
+++ src/face.h
@@ -3,6 +3,7 @@
 #include <glm/glm.hpp>
 #include <glad/glad.h>
 #include <string>
+#include <cuda_gl_interop.h>
 
 class GLSLProgram;
 
@@ -17,6 +18,8 @@ public:
 	Face& operator=(Face&&);
 	~Face();
 
+	//Copies m_face_data_gpu to content of m_vertex_buffer.
+	void updateVertexBuffer();
 	void draw(const GLSLProgram& program) const;
 
 private:
@@ -24,4 +27,10 @@ private:
 	GLuint m_vertex_buffer{ 0 };
 	GLuint m_index_buffer{ 0 };
 	unsigned int m_number_of_indices{ 0 };
+	unsigned int m_number_of_vertices{ 0 };
+
+	//CUDA-GL interop for buffer object.
+	cudaGraphicsResource* m_resource{ nullptr };
+	float* m_face_data_gpu{ nullptr };
+	int m_face_data_size{ 0 };
 };
