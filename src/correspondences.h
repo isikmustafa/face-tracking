1f2dd33efc261244f316da2f8a5117a4e5141ca8
Wojciech Zielonka <wojtas.zielonka@gmail.com>
2019-11-05 00:07:25 +0100

    Tracker code simplification

--- src/correspondences.h
+++ src/correspondences.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 class Correspondences
 {
+public:
+	std::vector<glm::vec2> target;
 };
--- src/tracker.cpp
+++ src/tracker.cpp
@@ -2,36 +2,36 @@
 #include <utility>
 
 Tracker::Tracker() {
-	dlib::deserialize("shape_predictor_68_face_landmarks.dat") >> m_pose_model;
-}
-
-Correspondences Tracker::getCorrespondences()
-{
-	if (!m_camera.isOpened()) {
-		return Correspondences();
-	}
-
 	try
 	{
-		cv::Mat temp;
-		if (!m_camera.read(temp))
-		{
-			return Correspondences();
-		}
-
-		dlib::cv_image<dlib::bgr_pixel> cimg(temp);
-		std::vector<dlib::rectangle> faces = m_detector(cimg);
-
-		std::vector<dlib::full_object_detection> shapes;
-		for (const auto& face : faces)
-		{
-			shapes.push_back(m_pose_model(cimg, face));
-		}
-
+		dlib::deserialize("shape_predictor_68_face_landmarks.dat") >> m_pose_model;
 	}
 	catch (dlib::serialization_error& e)
 	{
 		std::cout << std::endl << e.what() << std::endl;
+	}
+}
+
+Correspondences Tracker::getCorrespondences()
+{
+	if (!m_camera.isOpened())
+		return Correspondences();
+
+	try {
+		cv::Mat temp;
+		if (!m_camera.read(temp))
+			return Correspondences();
+
+		dlib::cv_image<dlib::bgr_pixel> cimg(temp);
+		std::vector<dlib::rectangle> faces = m_detector(cimg);
+
+		// Consider only one face for the processing
+		if (faces.empty())
+			return Correspondences();
+
+		dlib::full_object_detection shape = m_pose_model(cimg, faces[0]);
+
+		//TODO calculate Correspondences
 	}
 	catch (std::exception& e)
 	{
